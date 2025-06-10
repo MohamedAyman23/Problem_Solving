0b09aa14235c6ca8029812c5d22a031da35f8042
Mohammed Ayman <mohamedaymanmohamedatia@gmail.com>
2025-06-10 08:36:44 +0300

    This is the Linked List Class

---
+++ Java/LinkedList/src/main/java/newclass/linkedlist/Node.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package newclass.linkedlist;
+
+/**
+ *
+ * @author Mohammed Ayman
+ */
+public class Node <T>{
+    T value;
+    Node next;
+    public Node(T value, Node next){
+        this.value = value;
+        this.next = next;
+    }
+}
