b53611efa8eefedb8e7fe683260997d477f15e52
Mohammed Ayman <mohamedaymanmohamedatia@gmail.com>
2025-06-10 08:42:30 +0300

    Adding the Linked list functionality

--- Java/LinkedList/src/main/java/newclass/linkedlist/LinkedList.java
+++ Java/LinkedList/src/main/java/newclass/linkedlist/LinkedList.java
@@ -10,5 +10,23 @@ package newclass.linkedlist;
  * @author Mohammed Ayman
  */
 public class LinkedList {
-
+    Node head;
+    public LinkedList(Node head){
+        this.head = head;
+    }
+    public void add(Node newNode){
+        newNode.next = head;
+        head = newNode;
+    }
+    public void delete(){
+        head = head.next;
+    }
+    public void display(){
+        Node n = head;
+        while(n != null){
+            System.out.println(n.value);
+            n = n.next;
+        }
+        
+    }
 }
